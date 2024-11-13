8640fcc2924cfed1fbb6bcfe42b53f8aefa1d895
Abhinav Yadav <130930235+abhinavyadav11@users.noreply.github.com>
2024-11-13 18:44:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,22 @@ import streamlit as st
 import pandas as pd
 import pickle
 import requests
+import gdown
+import os
 
 
 # def fetch_poster(movie_id):
 #    response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=8265d1679663a7ea12ac168da84d2e8&language=en-US'.format(id))
 #    data = response.json()
 #    return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
+
+
+
+# Download the file from Google Drive if it doesn't exist
+file_path = 'similarity.pkl'
+if not os.path.exists(file_path):
+    url = 'https://drive.google.com/uc?id=1TZ9T5NiBwj0fBsu8HFBnb3xFbroEUspZ'
+    gdown.download(url, file_path, quiet=False)
 
 def recommend (movie):
     movie_index = movies[movies['title'] == movie].index[0]
@@ -21,6 +31,7 @@ def recommend (movie):
 
 movies_dict = pickle.load(open('movie-recommend-dict.pkl', 'rb'))
 movies = pd.DataFrame(movies_dict)
+
 
 similarity = pickle.load(open('similarity.pkl', 'rb'))
 
